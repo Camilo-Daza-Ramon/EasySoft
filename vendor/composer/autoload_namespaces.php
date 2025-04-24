9134b610cea27162c34fff3458b0b9f3fb3c1a1c
Camilo Daza <jcardona@sisteco.com.co>
2025-04-24 09:53:25 -0500

    Actualizacion de la documentacion en composer.json para que despliegue Render

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -10,7 +10,6 @@ return array(
     'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'PHPExcel' => array($vendorDir . '/phpoffice/phpexcel/Classes'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'Maatwebsite\\Excel\\' => array($vendorDir . '/maatwebsite/excel/src'),
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib'),
     'Bestnetwork\\Telnet\\' => array($vendorDir . '/bestnetwork/telnet'),
